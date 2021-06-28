05028c476aa4e06d4267addeaa8b5ec58f856327
rain <chenu9999@gmail.com>
2021-06-28 10:32:59 +0800

    c12-9自訂setter方法

--- Player.kt
+++ Player.kt
@@ -1,6 +1,10 @@
 class Player {
     val name = "madrigal"
         get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
+
 
     fun castFireball(numFireballs: Int = 2) =
         println("$numFireballs" + "杯Fireball突然出現")
