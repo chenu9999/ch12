790c39c7bb898d35e1ad05317c879b6972e39217
rain <chenu9999@gmail.com>
2021-06-28 11:01:45 +0800

    c12-18從main裡刪除幾個函數

--- Game.kt
+++ Game.kt
@@ -3,8 +3,6 @@ fun main() {
     player.castFireball()
 
     //光環
-    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20 ).toInt()
-    //val auraVisible = isBlessed && healthPoints > 50 || isImmortal 用不到
     val auraColor = auraColor(karma)
 
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
@@ -24,28 +22,6 @@ private fun printPlayerStatus(
             "(運氣:${if (isBlessed) "好" else "壞"}) -> $name $healthStatus"
     println(statusFormatString)
 }
-
-private fun auraColor(karma: Int) =
-    when (karma) {
-        in 16..20 -> "綠色光環"
-        in 11..15 -> "紫色光環"
-        in 6..10 -> "橘黃色光環"
-        in 0..5 -> "紅色光環"
-        else -> "無光環"
-    }
-
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> "狀態極佳"
-        in 90..99 -> "有一些小擦傷"
-        in 75..89 -> if (isBlessed) {
-            "雖有一些傷口，但恢復很快"
-        } else {
-            "有一些傷口"
-        }
-        in 15..74 -> "嚴重受傷"
-        else -> "狀態不妙"
-    }
 
 /*private fun drink(glass: Int) {
     val drunkenness = when (glass) {
